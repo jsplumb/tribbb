f0cb10314eb893fe401c624a5f97efc3d22a50a8
sporritt <simon.porritt@gmail.com>
2018-05-01 09:14:42 +1000

    expand test coverage a little

--- test/test.js
+++ test/test.js
@@ -7,11 +7,23 @@
             data:TestJSON
         });
 
+        equal(index.getDocumentCount(), 10, "there are 10 docs in the indexer");
+
         var docs = index.search("A2");
         equal(docs.length, 1, "1 doc found");
+        equal(docs[0].document.name, "A2", "name of first document matches");
 
         docs = index.search("A");
         equal(docs.length, 10, "10 docs found");
+        equal(docs[0].document.name, "A2", "name of first document matches");
+        equal(docs[1].document.name, "A0100B", "name of second document matches");
+
+        index.remove(docs[0].document);
+        docs = index.search("A2");
+        equal(docs.length, 0, "0 docs found");
+
+        equal(index.getDocumentCount(), 9, "there are now 9 docs in the indexer");
+
     });
 
 }).call(typeof window === "undefined" ? this : window);
